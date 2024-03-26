0b2e1f74b109e381674b14d50f13728b58348a0b
barelywinning <143749723+barelywinning@users.noreply.github.com>
2024-03-26 19:52:32 +0530

    Update work1.py

--- work1.py
+++ work1.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 car_price = pd.read_csv(r"C:\Users\adity\Downloads\CarPrices\CarPrice_Assignment.csv")
 data = car_price.drop(["CarName","enginelocation", "fuelsystem"], axis=1)
